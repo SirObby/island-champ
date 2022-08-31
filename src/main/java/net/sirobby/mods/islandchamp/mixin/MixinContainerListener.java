69dcc8759b654951d4395dad5c9692db0d0aad17
VeryHolyCheeeese <79263404+VeryHolyCheeeese@users.noreply.github.com>
2022-08-31 21:30:01 +0800

    wot

--- src/main/java/net/sirobby/mods/islandchamp/mixin/MixinContainerListener.java
+++ src/main/java/net/sirobby/mods/islandchamp/mixin/MixinContainerListener.java
@@ -1,21 +1,23 @@
 package net.sirobby.mods.islandchamp.mixin;
 
+import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.GenericContainerScreenHandler;
+import net.sirobby.mods.islandchamp.IslandChamp;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GenericContainerScreenHandler.class)
+@Mixin(GenericContainerScreen.class)
 public class MixinContainerListener {
 
-    @Shadow
-    @Final
-    private Inventory inventory;
-    @Shadow
-    @Final
-    private int rows;
-
-
+    @Inject(method = {"render"}, at = @At("HEAD"))
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        
+    }
 
 }
